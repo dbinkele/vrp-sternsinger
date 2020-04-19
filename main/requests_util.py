7c171246f3aa3e196cd1f78e928ea2c5aaa27098
dbinkele <dbinkele@itemis.de>
2020-04-19 06:27:22 +0200

    Refactoring

--- main/requests_util.py
+++ main/requests_util.py
@@ -3,7 +3,8 @@ import os
 from functools import reduce
 
 import requests
-from numpy.distutils.system_info import lapack_opt_info
+
+OPEN_ROUTES_URL = "https://api.openrouteservice.org/v2/matrix/foot-walking"
 
 OSRM_QUERY = 'http://127.0.0.1:5000/table/v1/walking/{}'
 DISTANCE_MATRIX_QUERY = OSRM_QUERY  # MAPBOX_QUERY
@@ -11,6 +12,9 @@ MAPBOX_QUERY = 'https://' + 'api.mapbox.com' + '/directions-matrix/v1/mapbox/wal
                                                '}?access_token=pk' \
                                                '.eyJ1IjoiZGFuZGVsaW4iLCJhIjoiY2s0NzFybHJqMGFpYTNrcWxmanp2b2tzcyJ9.XQN' \
                                                '-K3gC4ON6D75A361b1g'
+
+HEADER = {"Authorization": "Bearer {}", 'Accept-Encoding': 'UTF-8', 'Content-Type': 'application/json',
+          'Accept': '*/*'}
 
 
 def request_dist_matrix(adresses_json):
@@ -20,14 +24,16 @@ def request_dist_matrix(adresses_json):
 
 def request_remote(adresses_json):
     api_key = os.getenv("MAP_API_KEY")
-
-    endpoint = "https://api.openrouteservice.org/v2/matrix/foot-walking"
-    lon_lat = [[item['lon'], item['lat']] for item in adresses_json]
     data = json.dumps(
-        {"locations": lon_lat})
+        {"locations": [[item['lon'], item['lat']] for item in adresses_json]})
+
+    return requests.post(OPEN_ROUTES_URL, data=data, headers=(header(api_key))).json()
+
+
+def header(api_key):
     headers = {"Authorization": "Bearer " + api_key, 'Accept-Encoding': 'UTF-8', 'Content-Type': 'application/json',
                'Accept': '*/*'}
-    return requests.post(endpoint, data=data, headers=headers).json()
+    return headers
 
 
 def request_local(adresses_json):
