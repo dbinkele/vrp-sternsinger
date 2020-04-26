4163c33a366ee97a1518babc38084c70a38de980
dbinkele <dbinkele@itemis.de>
2020-04-26 07:24:54 +0200

    Adds async processing with rq

--- main/app.py
+++ main/app.py
@@ -1,17 +1,32 @@
 import os
 
 from flask import request, Flask, jsonify
+from rq import Queue
 
 from main.worker.job import run_job
+from main.worker.worker import conn
 
 app = Flask(__name__, instance_relative_config=True)
+config_file = os.environ.get('CONFIG_FILE')
+app.config.from_pyfile(config_file)
+q = Queue(connection=conn)
+
+# RECIPENT = 'dbinkeleraible@googlemail.com'
+RECIPENT = 'dbinkele@itemis.com'
+
+MAIL_KEYS = ['MAIL_SERVER', 'MAIL_PORT', 'MAIL_USERNAME', 'MAIL_PASSWORD', 'MAIL_USE_SSL',
+             'MAIL_USE_TLS']
 
 
 @app.route("/", methods=["POST"])
 def handle_job():
     data = request.get_json()
-    result = run_job(data['data'], data['constraints'])
-    return jsonify(result)
+    q.enqueue(run_job, data['data'], data['constraints'], mail_config(), "")
+    return jsonify("{'success': 1}")
+
+
+def mail_config():
+    return dict((k, app.config[k]) for k in MAIL_KEYS if k in app.config)
 
 
 if __name__ == "__main__":
--- main/worker/job.py
+++ main/worker/job.py
@@ -1,10 +1,44 @@
+import smtplib
+from email.message import EmailMessage
+
 from main.requests_util import request_dist_matrix
 from main.run_algorithm import mainrunner
 from main.template import render
 
 
-def run_job(address_json, constrains_json):
+def run_job(address_json, constrains_json, config, mail_to):
     dist_matrix_json = request_dist_matrix(address_json)
     json_routes = mainrunner(dist_matrix_json, constrains_json, address_json)
     routes_html = [render(json_route) for json_route in json_routes]
-    return routes_html
+    print(routes_html)
+
+    msg = EmailMessage()
+    msg['From'] = config['MAIL_USERNAME']
+    msg['To'] = mail_to
+    msg['Subject'] = 'Just Testing..'
+    message = """{}""".format(routes_html)
+    msg.set_content(message)
+
+    with make_server(config) as server:
+        if config.get('MAIL_PASSWORD'):
+            server.login(config['MAIL_USERNAME'], config.get('MAIL_PASSWORD'))
+        server.send_message(msg)
+        server.quit()
+
+    return {'message': message}
+
+
+def make_server(config):
+    server = config['MAIL_SERVER']
+    port = config['MAIL_PORT']
+    if config.get('MAIL_USE_SSL'):
+        client = smtplib.SMTP_SSL(server, port)
+    else:
+        client = smtplib.SMTP(server, port)
+
+    if config.get('MAIL_USE_TLS'):
+        client.ehlo()
+        client.starttls()
+        client.ehlo()
+
+    return client
